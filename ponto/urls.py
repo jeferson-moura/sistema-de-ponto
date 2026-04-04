3a1305ca827b0047d200967d776d106bdd92f7a5
Jeferson <diegorealiza26@gmail.com>
2026-04-04 14:16:00 -0300

    fix login definitivo

--- ponto/urls.py
+++ ponto/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+
+    # LOGIN PADRÃO DO DJANGO
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
     path('relatorio/', views.relatorio, name='relatorio'),
     path('exportar-excel/', views.exportar_excel, name='exportar_excel'),
     path('gestao/', views.dashboard_gestao, name='dashboard_gestao'),
